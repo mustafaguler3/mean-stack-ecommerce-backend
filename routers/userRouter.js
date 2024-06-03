4142feec1929d783d023319c31a1f62c5679956e
mustafaguler3 <mustafa.glr@hotmail.com>
2024-06-03 14:26:41 +0300

    get user and list of users endpoint

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,14 +1,33 @@
 const express = require("express")
 const router = express.Router();
 const User = require("../models/user")
+const hash= require("bcryptjs")
 
+router.get("/",async (req,res) => {
+    const userList = await User.find().select("name street")
+
+    if(!userList){
+        return res.status(500).json({success: false})
+    }
+
+    res.status(200).send(userList)
+})
+
+router.get("/:id",async (req,res) => {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if(!user){
+        return res.status(500).json("user not found")
+    }
+    res.send(user)
+})
 
 router.post("/",async (req,res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
         street: req.body.street,
-        password: req.body.password,
+        password: hash.hashSync(req.body.password,10),
         phone: req.body.phone,
         isAdmin : req.body.isAdmin,
         apartment: req.body.apartment,
